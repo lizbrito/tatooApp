9eeb96b505bf693974934d2a3aa819f1420ebe72
lizbrito <lizcostafernandes@hotmail.com>
2021-08-16 22:36:23 -0700

    addint tasks and x icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
+const App = () => {
+  const [tasks, setTasks] = useState([        
+    {
+      id: 1,
+      text: 'Doctors Appoitment',
+      day: 'Feb 5th at 9:30',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Rag Vet',
+      day: 'Feb 17th at 14:30',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Groceries',
+      day: 'Mar 9th at 17:30',
+      reminder: false,
+    },        
+  ])
 
-function App() {
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
-import Tasks from '../Tasks';
+import Tasks from './Tasks';
 
 const Header = ({ title }) => {
     const onClick = () => {
@@ -10,13 +10,9 @@ const Header = ({ title }) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
-            <Tasks />
-        
+            <Button color='green' text='Add' onClick={onClick}/>     
         </header>
     )
-
-
 }
 
 Header.defaultProps = {
